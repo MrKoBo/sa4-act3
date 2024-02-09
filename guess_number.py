38f79d7f6673175fb2408871bcb59cd7f0eff43b
MrKoBo <kdtate4@memphis.edu>
2024-02-09 02:58:08 -0600

    Added a hardcoded limit to the amount of guesses the user gets

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,10 @@
 number = 10
+guesses = 5
 
 print("I'm thinking of a number...")
 correct = False
-guess = input("What number am I thinking of? (If you give up press q) ")
+guess = input(f"You have {guesses} guesses. What number am I thinking of? (If you give up press q) ")
+guesses -= 1
 while not correct and guess != 'q':
     
     if guess != "q":
@@ -10,8 +12,12 @@ while not correct and guess != 'q':
     if guess == number:
         print("Congratulations! You guessed the right number.")
         break
-    else:
-        guess = input("Sorry, that is not right try again! ")
+    elif guesses > 0:
+        guess = input(f"Uh oh! you now have {guesses} many guesses left. Try again! ")
+        guesses -= 1
+    if guesses == 0:
+        print(f"Sorry the correct answer was {number} you have ran out of guesses.")
+        break
 else:
     
     print(f"Sorry, the correct answer was {number}")
