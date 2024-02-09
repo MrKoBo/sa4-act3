1855f45eaad081c737506fcca304c339bfe2354b
MrKoBo <kdtate4@memphis.edu>
2024-02-09 02:41:36 -0600

    Added loop to allow user to continue guessing or quit

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,17 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
-
-if guess == number:
-   print("Congratulations! You guessed the right number.")
+correct = False
+guess = input("What number am I thinking of? (If you give up press q) ")
+while not correct and guess != 'q':
+    
+    if guess != "q":
+        guess = int(guess)
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        guess = input("Sorry, that is not right try again! ")
 else:
-   print(f"Sorry! The number was {number}.")
+    
+    print(f"Sorry, the correct answer was {number}")
